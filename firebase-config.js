e80a0b4c8499024f45393c98df4fd756aa20b7dc
rosai868 <rosai041321@gmail.com>
2025-11-19 11:54:44 +0900

    Create firebase-config.js

---
+++ firebase-config.js
@@ -0,0 +1,9 @@
+// この中に、Firebaseコンソールからコピーした設定を貼り付けます
+const firebaseConfig = {
+  apiKey: "AIzaSyB76MVgiAj7tL0ty5qgMT_kGutOwFw-WaY",
+  projectId: "shift-management-system-bc2ac",
+  storageBucket: "Yshift-management-system-bc2ac.firebasestorage.app",
+  messagingSenderId: "482437174317",
+  appId: "1:482437174317:web:64aec1dd06db66625fd4a4",
+  databaseURL: "https://shift-management-system-bc2ac-default-rtdb.firebaseio.com" // Realtime Databaseを使っている場合
+};
